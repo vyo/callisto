7487bc218435f49287a74da62ba3f05a2091afcf
Manuel Weidmann <weidmann.manuel@gmail.com>
2015-10-15 21:58:22 +0200

    Add insecure param to stack constructor
     - efficient stack manipulation requires direct access to the
       backing data structure
     - if insecure mode is specified stack table and top pointer
       are exposed

--- lib/stack.lua
+++ lib/stack.lua
@@ -1,14 +1,18 @@
-local function Stack()
+local function Stack (insecure)
   local self = {}
   local stack = {}
   local top = 0
+  if (insecure) then
+    self.stack = stack
+    self.top = top
+  end
  
   function self.push (item)
     top = top + 1
     stack[top] = item
   end
 
-  function self.pop()
+  function self.pop ()
     local item = stack[top]
     if (item == nil) then
       error('Trying to retrieve from empty stack.')
