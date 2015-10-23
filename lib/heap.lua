324177dcd89a4985b1be04f4d7e7e1a19196d7ca
Manuel Weidmann <weidmann.manuel@gmail.com>
2015-10-23 08:49:18 +0200

    Finalise Heap tests

--- lib/heap.lua
+++ lib/heap.lua
@@ -2,11 +2,23 @@ local function Heap ()
   local self = {}
   local heap = {}
 
+  self.ERROR_STORE_INVALID_KEY = 'Trying to store nil key'
+  self.ERROR_STORE_INVALID_VALUE = 'Trying to store nil value'
+  self.ERROR_RETRIEVE_INVALID_KEY = 'Trying to retrieve nil key'
+  
   function self.store (key, value)
+    if key == nil then
+      error(self.ERROR_STORE_INVALID_KEY)
+    elseif value == nil then
+      error(self.ERROR_STORE_INVALID_VALUE)
+    end
     heap[key] = value
   end
 
   function self.retrieve (key)
+    if key == nil then
+      error(self.ERROR_RETRIEVE_INVALID_KEY)
+    end
     return heap[key]
   end
 
--- test/heap_spec.lua
+++ test/heap_spec.lua
@@ -1,17 +1,45 @@
 describe('Heap module', function ()
 
   local heap = require 'heap'
-  heap = heap()
 
-  it('should expose push and pop functions', function ()
-    pending('')
+  setup(function ()
+    heap = heap()
   end)
+
+  it('should expose store and retrieve functions', function ()
+    assert.are.equals('function', type(heap.store))
+    assert.are.equals('function', type(heap.retrieve))
+  end)
+
   it('should allow to store elements by key', function ()
-    pending('')
+    assert.has_no.errors(heap.store, 'key', 'value')
+  end)
+
+  it('should not allow to store nil keys', function ()
+    local test = function ()
+      heap.store(nil, 'value')
+    end
+    assert.has_error(test, heap.ERROR_STORE_INVALID_KEY)
+  end)
+
+  it('should not allow to store nil values', function ()
+    local test = function ()
+      heap.store('key', nil)
+    end
+    assert.has_error(test, heap.ERROR_STORE_INVALID_VALUE)
   end)
 
   it('should allow to retrieve elements by key', function ()
-    pending('')
+    heap.store('key', 'value')
+    assert.has_no.errors(heap.retrieve, 'key')
+    assert.are.equals('value', heap.retrieve('key'))
   end)
-  
+ 
+  it('should not allow to retrieve nil keys', function ()
+    local test = function ()
+      heap.retrieve(nil)
+    end
+    assert.has_error(test, heap.ERROR_RETRIEVE_INVALID_KEY)
+  end)
+
 end)
